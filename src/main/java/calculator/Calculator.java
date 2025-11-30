0b653159635d823cf35ccdc4542ff4644f034446
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-11-30 16:23:43 +0200

    Update calculator to use Logger

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,10 @@
 package calculator;
 
+import java.util.logging.Logger;
+
 public class Calculator {
+
+    Logger logger = Logger.getLogger(Calculator.class.getName());
 
     public double sumNumbers(double a, double b) {
         return a + b;
@@ -16,7 +20,7 @@ public class Calculator {
 
     public double divNumbers(double a, double b) {
         if (b ==0) {
-            System.out.println("Error: Division by zero");
+            logger.warning("Division by zero attempted.");
             return 0;
         } else {
             return a / b;
--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,24 +1,34 @@
 package calculator;
 
+import java.util.logging.Logger;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Logger logger = Logger.getLogger(Main.class.getName());
         Calculator cal = new Calculator();
 
+        String result = "Hello, Calculator!";
+        logger.info(() -> result);
         double summation = cal.sumNumbers(4, 6);
-        System.out.println(summation);
+        String summationStr = "The sum of 4 and 6 is: " + summation;
+        logger.info(() -> String.format(summationStr));
 
         double subtraction = cal.subNumber(8, 3);
-        System.out.println(subtraction);
+        String subtractionStr = "The subtraction of 8 and 3 is: " + subtraction;
+        logger.info(() -> String.format(subtractionStr));
 
         double multiplication = cal.multNumbers(5, 7);
-        System.out.println(multiplication);
+        String multiplicationStr = "The multiplication of 5 and 7 is: " + multiplication;
+        logger.info(() -> String.format(multiplicationStr));
 
         double division = cal.divNumbers(20, 4);
-        System.out.println(division);
+        String divisionStr = "The division of 20 by 4 is: " + division;
+        logger.info(() -> String.format(divisionStr));
 
         double divisionByZero = cal.divNumbers(10, 0);
-        System.out.println(divisionByZero);
+        String divisionByZeroStr = "The division of 10 by 0 is: " + divisionByZero;
+        logger.info(() -> String.format(divisionByZeroStr));
     }
 }
